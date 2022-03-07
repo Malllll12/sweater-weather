63eec126b24bad476867015ea1ea6f2d53171712
Mallory Vining <mallorygallery12@gmail.com>
2022-03-07 10:38:23 -0700

    Add book search

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :forecast, only: :index
       resources :backgrounds, only: :index
+
+      get "/book-search", to: 'book#index'
     end
   end
 end
