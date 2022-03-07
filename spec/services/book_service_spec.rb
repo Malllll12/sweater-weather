0ca1872da3518a5bb09996ffb320c8e4e50e4aa7
Mallory Vining <mallorygallery12@gmail.com>
2022-03-07 09:52:23 -0700

    Add book service

---
+++ spec/services/book_service_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe BookService do
+  it "returns book data", :vcr do
+    data = BookService.get_books("Telluride,CO", 5)
+    expect(data).to be_a(Hash)
+    expect(data[:docs]).to be_an Array
+    expect(data[:docs][0]).to have_key(:title)
+    expect(data[:docs][0]).to have_key(:isbn)
+    expect(data[:docs][0]).to have_key(:publisher)
+  end
+end
