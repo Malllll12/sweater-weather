549e1678db5a9dfbefa6619624791c15a8059827
Mallory Vining <mallorygallery12@gmail.com>
2022-03-07 11:39:16 -0700

    Fix Serializer

--- app/serializers/books_serializer.rb
+++ app/serializers/books_serializer.rb
@@ -1,36 +1,15 @@
 class BooksSerializer
-  def self.serialize
-    {
-  data: {
-    id: "null",
-    type: "books",
-    attribute: {
-      destination: "denver,co",
-      forecast: {
-        summary: "Cloudy with a chance of meatballs",
-        temperature: "83 F"
-      },
-      total_books_found:172,
-      books: [
-        {
-          isbn: [
-            "0762507845",
-            "9780762507849"
-          ],
-          title: "Denver, Co",
-          publisher: [
-            "Universal Map Enterprises"
-          ]
-        },
-        {
-          isbn: [
-            "9780883183663",
-            "0883183668"
-          ],
-          title: "Photovoltaic safety, Denver, CO, 1988",
-          publisher: [
-            "American Institute of Physics"
-          ]
-        }
+  def self.serialize(data, location, weather)
+    { data: {
+            id: "null",
+            type: "books",
+            attribute: {
+                        destination: location,
+                        forecast: {
+                                    summary: weather.conditions,
+                                    temperature: weather.temperature
+                                  },
+                        total_books_found: data[:total_books_found],
+                        books: data[:books]}}}
   end
 end
