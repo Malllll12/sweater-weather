d2672b7a6f0c4fbee626f0fecb212e1e2d0f966a
Mallory Vining <mallorygallery12@gmail.com>
2022-03-07 18:54:18 -0700

    Magic commit

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,17 +1,15 @@
 class ForecastSerializer
-  class << self
-    def serialize(data)
-      {
-        data: {
-              id: "null",
-              type: 'forecast',
-              attributes: {
-                  current_weather: data[:current_weather],
-                  daily_weather: data[:daily_weather],
-                  hourly_weather: data[:hourly_weather]
-                          }
-              }
-      }
-    end
+  def self.serialize(data)
+    {
+      data: {
+            id: "null",
+            type: 'forecast',
+            attributes: {
+                current_weather: data[:current_weather],
+                daily_weather: data[:daily_weather],
+                hourly_weather: data[:hourly_weather]
+                        }
+            }
+    }
   end
 end
