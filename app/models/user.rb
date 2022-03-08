11630a0456858cd18ce169a84c4039448bae4f64
Mallory Vining <mallorygallery12@gmail.com>
2022-03-07 19:00:01 -0700

    Magic commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  # before_create :api_key_generate
-
   def self.api_key_generate
     randomizer = SecureRandom.base64.tr('+/=', 'Qrt')
   end
