d683f238e86f3ebeec25fbb9b048564a02daec6a
Mallory Vining <mallorygallery12@gmail.com>
2022-03-08 15:30:08 -0700

    Cleanup serializers

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -8,8 +8,8 @@ class ForecastSerializer
                 current_weather: data[:current_weather],
                 daily_weather: data[:daily_weather],
                 hourly_weather: data[:hourly_weather]
-                        }
+                }
+              }
             }
-    }
   end
 end
