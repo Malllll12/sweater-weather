8bf407693ca56cf62f69644f83a99efaa1f5e842
Mallory Vining <mallorygallery12@gmail.com>
2022-03-08 15:16:24 -0700

    Create trip poros

---
+++ app/poros/trip.rb
@@ -0,0 +1,8 @@
+class Trip
+  attr_reader :temperature,
+              :conditions
+  def initialize(data)
+    @temperature = data[:temp]
+    @conditions = data[:weather][0][:description]
+  end
+end
