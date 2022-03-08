da9db2ae4a7697868f7e90472e769bf3b6b71e89
Mallory Vining <mallorygallery12@gmail.com>
2022-03-07 18:53:34 -0700

    Add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+require 'bcrypt'
 class User < ApplicationRecord
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_presence_of :password, confirmation: true
+
+  has_secure_password
+
+  # before_create :api_key_generate
+
+  def self.api_key_generate
+    randomizer = SecureRandom.base64.tr('+/=', 'Qrt')
+  end
 end
