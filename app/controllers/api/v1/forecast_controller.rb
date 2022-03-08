ffcdc61d93c14b63093ac47fb0cb103b9d84428d
Mallory Vining <mallorygallery12@gmail.com>
2022-03-07 20:12:17 -0700

    add sad path test

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    coords = LocationFacade.get_location(params[:location])
-    all = {
-         current_weather: WeatherFacade.current_weather(coords.latitude, coords.longitude),
-         daily_weather: WeatherFacade.daily_weather(coords.latitude, coords.longitude),
-         hourly_weather: WeatherFacade.hourly_weather(coords.latitude, coords.longitude)
-        }
-    render json: ForecastSerializer.serialize(all)
+    if params[:location].empty?
+      render json: { error: "Location must be given"}, status: 404
+    else
+      coords = LocationFacade.get_location(params[:location])
+      all = {
+        current_weather: WeatherFacade.current_weather(coords.latitude, coords.longitude),
+        daily_weather: WeatherFacade.daily_weather(coords.latitude, coords.longitude),
+        hourly_weather: WeatherFacade.hourly_weather(coords.latitude, coords.longitude)
+      }
+      render json: ForecastSerializer.serialize(all), status: 200
+    end
   end
 end
