da419a260c1737821def0b4869b5760be4b175bc
Mallory Vining <mallorygallery12@gmail.com>
2022-03-09 09:01:15 -0700

    Add test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,11 @@ describe User, type: :model do
     it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
   end
+
+  describe "methods" do
+    it "generatates a key" do
+      user = User.create(email: "fake@example.com", password: "seacret", password_confirmation: "seacret", api_key: User.api_key_generate)
+      expect(user.api_key).to be_a(String)
+    end
+  end
 end
