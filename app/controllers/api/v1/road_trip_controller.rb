fea83a915208fc7b60a6fce9eb9f2e7193fb3724
Mallory Vining <mallorygallery12@gmail.com>
2022-03-08 19:42:08 -0700

    Magic commit

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RoadTripController < ApplicationController
   def create
     if user = User.find_by(api_key: params[:api_key])
       if params[:origin].nil? || params[:destination].nil?
-        render json: { errors: 'Route cannot exist' }, status: 400
+        render json: { errors: "Route cannot exist" }, status: 400
       elsif
         time = LocationFacade.get_trip(params[:origin], params[:destination])
         coords = LocationFacade.get_location(params[:destination])
@@ -12,7 +12,7 @@ class Api::V1::RoadTripController < ApplicationController
         render json: RoadTripSerializer.impossible(params[:destination], params[:origin]), status: 200
       end
     else
-      render json: { errors: 'Please use correct credentials' }, status: 401
+      render json: { errors: "Please use correct credentials"}, status: 401
     end
   end
 end
